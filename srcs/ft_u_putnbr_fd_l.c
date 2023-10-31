6e7ff2c222b70caee4d757abe0b02369cc8cf1a2
Marius Van Pee <mvan-pee@fu-r1-p5.s19.be>
2023-10-31 17:22:30 +0100

    norm error

--- srcs/ft_u_putnbr_fd_l.c
+++ srcs/ft_u_putnbr_fd_l.c
@@ -6,15 +6,15 @@
 /*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 14:40:50 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/10/31 14:56:08 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/10/31 17:22:18 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int ft_u_int_len(unsigned int a)
+int	ft_u_int_len(unsigned int a)
 {
-    int	len;
+	int	len;
 
 	len = 1;
 	if (a < 0)
@@ -32,12 +32,13 @@ int ft_u_int_len(unsigned int a)
 
 int	ft_u_putnbr_fd_l(int n, int fd)
 {
-	int	len;
-    unsigned int a;
-    if(n < 0)
-        a = (UINT_MAX + n) + 1;
-    else
-        a = n;
+	int				len;
+	unsigned int	a;
+
+	if (n < 0)
+		a = (UINT_MAX + n) + 1;
+	else
+		a = n;
 	len = ft_u_int_len(a);
 	if (a >= 10)
 		ft_putnbr_fd_l(a / 10, fd);
